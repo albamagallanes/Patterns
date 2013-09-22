82dcc836383f06161ca9a594c0eaae1eae4ab946
albamagallanes <albamagallanes@gmail.com>
2013-09-22 10:23:38 -0500

    Update Image.java

--- Tarea/src/Image.java
+++ Tarea/src/Image.java
@@ -15,8 +15,6 @@ import net.sf.image4j.codec.bmp.BMPDecoder;
 
 public class Image {
 
-kaspfuabejfn 1928473210573328!@$!#$^&
-
 	static ArrayList<Pixel> pixels = new ArrayList<Pixel>();
 	static ArrayList<Pixel> pixelsTemp = new ArrayList<Pixel>();
 	Pixel [][] publicMatrix = null;
